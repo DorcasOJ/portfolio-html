01798dc751759e2554d2919ba23494b7e702d073
Dorcas <ayanfedorcas2017@gmail.com>
2025-04-29 10:03:44 +0100

    update

--- utils/inputs.js
+++ utils/inputs.js
@@ -1,62 +1,57 @@
-
-
 const inputs = {
-    blogs : [
-        {
-            image_url: 'https://i.postimg.cc/dLt0h2X3/command-line-blog.jpg',
-            image_title: 'Using the Command-Line and Shell Scriptingng up Nestjs with PostgreSQL',
-            image_description: "A Command Line Interface (CLI) helps you interact with your system by...",
-            image_link: "https://medium.com/dev-genius/using-the-command-line-and-shell-scripting-1f0e84d76a8e"
-        },
-        
-        {
-            image_url: 'https://i.postimg.cc/QMzxBkC9/python-intro-blog.jpg',
-            image_title: 'Introduction to Python',
-            image_description: " If you are new to programming or you want to learn how to write python then this is for you...",
-            image_link: "https://medium.com/@dorcas2017/introduction-to-python-2eb51cf91b83"
-        },
-        {
-            image_url: 'https://i.postimg.cc/m2LmbcpF/nestjs-blog.jpg',
-            image_title: ' Setting up Nestjs with PostgreSQL',
-            image_description: "NestJS is a framework used to create server-side NodeJS applications. I recently started working with NestJs and...",
-            image_link: "https://medium.com/dev-genius/setting-up-nestjs-with-postgresql-ac2cce9045fe"
-        },
-        {
-            image_url: 'https://i.postimg.cc/m2LmbcpF/nestjs-blog.jpg',
-            image_title: 'Setting up Nestjs with PostgreSQL',
-            image_description: "NestJS is a framework used to create server-side NodeJS applications. I recently started working with NestJs and...",
-            image_link: "https://medium.com/dev-genius/setting-up-nestjs-with-postgresql-ac2cce9045fe"
-        },
-        
-    ],
+  blogs: [
+    {
+      image_url: "https://i.postimg.cc/dLt0h2X3/command-line-blog.jpg",
+      image_title:
+        "Using the Command-Line and Shell Scriptingng up Nestjs with PostgreSQL",
+      image_description:
+        "A Command Line Interface (CLI) helps you interact with your system by...",
+      image_link:
+        "https://medium.com/dev-genius/using-the-command-line-and-shell-scripting-1f0e84d76a8e",
+    },
 
-    projects : [
-        {
-            image_url: 'https://i.postimg.cc/D8HSJCX6/sample-to-do.png',
-            image_title: 'To Do App',
-            image_description: "To create and track daily Tasks",
-            image_link: "https://dorcaz-todo.netlify.app/"
-        },
-        {
-            image_url: 'https://i.postimg.cc/D8HSJCX6/sample-to-do.png',
-            image_title: 'To Do App',
-            image_description: "To create and track daily Tasks",
-            image_link: "https://dorcaz-todo.netlify.app/"
-        },
-        {
-            image_url: 'https://i.postimg.cc/D8HSJCX6/sample-to-do.png',
-            image_title: 'To Do App',
-            image_description: "To create and track daily Tasks",
-            image_link: "https://dorcaz-todo.netlify.app/"
-        },
-        {
-            image_url: 'https://i.postimg.cc/D8HSJCX6/sample-to-do.png',
-            image_title: 'To Do App',
-            image_description: "To create and track daily Tasks",
-            image_link: "https://dorcaz-todo.netlify.app/"
-        },
-        
-    ]
+    {
+      image_url: "https://i.postimg.cc/QMzxBkC9/python-intro-blog.jpg",
+      image_title: "Introduction to Python",
+      image_description:
+        " If you are new to programming or you want to learn how to write python then this is for you...",
+      image_link:
+        "https://medium.com/@dorcas2017/introduction-to-python-2eb51cf91b83",
+    },
+    {
+      image_url: "https://i.postimg.cc/m2LmbcpF/nestjs-blog.jpg",
+      image_title: " Setting up Nestjs with PostgreSQL",
+      image_description:
+        "NestJS is a framework used to create server-side NodeJS applications. I recently started working with NestJs and...",
+      image_link:
+        "https://medium.com/dev-genius/setting-up-nestjs-with-postgresql-ac2cce9045fe",
+    },
+    {
+      image_url: "https://i.postimg.cc/m2LmbcpF/nestjs-blog.jpg",
+      image_title: "Setting up Nestjs with PostgreSQL",
+      image_description:
+        "NestJS is a framework used to create server-side NodeJS applications. I recently started working with NestJs and...",
+      image_link:
+        "https://medium.com/dev-genius/setting-up-nestjs-with-postgresql-ac2cce9045fe",
+    },
+  ],
+
+  projects: [
+    {
+      image_url:
+        "https://i.postimg.cc/0zjbXC3Y/Screenshot-2025-04-29-100131.png",
+      image_title: "Netflix App",
+      image_description:
+        "View, search and watch your favorite Netflix movie trailers",
+      image_link: "https://netflix-clone-e75b8.web.app/",
+    },
+    {
+      image_url: "https://i.postimg.cc/D8HSJCX6/sample-to-do.png",
+      image_title: "To Do App",
+      image_description: "To create and track daily Tasks",
+      image_link: "https://dorcaz-todo.netlify.app/",
+    },
+  ],
 };
 
 export default inputs;
